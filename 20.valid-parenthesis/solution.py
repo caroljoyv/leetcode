a2c06b2288550aa0bf9370ca47a2f36fd53aa14e
Carolene Joy <caroljoyv27@gmail.com>
2023-09-26 22:30:43 +0530

    update 20.valid parenthesis better approach

--- 20.valid-parenthesis/solution.py
+++ 20.valid-parenthesis/solution.py
@@ -1,28 +1,49 @@
 s = "]"
 
+# def find(s):
+#     dict = {
+#         ')':'(',
+#         '}':'{',
+#         ']':'['
+#     }
+#     stack =[]
+#     for i in s:
+#         if i =='(' or i=='{' or i =='[':
+#             stack.append(i)
+#         elif i == ')' or i == '}' or i == ']':
+#             if stack:
+#                 ele = stack.pop()
+#             else: 
+#                 return False
+#             if dict[i] != ele:
+#                 print("inside for loop")
+#                 return False
+            
+#     if stack:
+#         return False
+#     else:
+#         return True
+
 def find(s):
+    stack = []
     dict = {
         ')':'(',
         '}':'{',
         ']':'['
     }
-    stack =[]
+
     for i in s:
-        if i =='(' or i=='{' or i =='[':
+        if i in dict:
+            if stack and stack[-1] == dict[i]:
+                stack.pop()
+            else:
+                return False
+        else:
             stack.append(i)
-        elif i == ')' or i == '}' or i == ']':
-            if stack:
-                ele = stack.pop()
-            else: 
-                return False
-            if dict[i] != ele:
-                print("inside for loop")
-                return False
-            
-    if stack:
-        return False
-    else:
-        return True
+    return True if not stack else False
+        
+    
+
 
 
 
