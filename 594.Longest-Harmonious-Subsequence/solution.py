66ee47aa1c6cb94554b4e085eeba4f37d40ef53b
caroljoyv <caroljoyv27@gmail.com>
2025-07-09 01:01:33 +0530

    add optimal solution

--- 594.Longest-Harmonious-Subsequence/solution.py
+++ 594.Longest-Harmonious-Subsequence/solution.py
@@ -30,3 +30,31 @@ def findLHS(nums:[int]) -> int:
 
 print(findLHS([1,3,2,2,5,2,3,7]))
 print(findLHS([1,2,3,4]))
+
+
+# Approach 2: better one
+
+# a. create a hashmap with value and frequency
+# b. for each unique value, check if n+1 exists in the map
+# c. if yes, max lenght = max of max_lenght and current lenght, where current length = freq of n + freq of n+1
+
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+
+from collections import Counter
+
+def solutionUsingHashMaps(nums:[int]) -> int:
+        max_length = 0
+        frequencyMap = Counter(nums)
+
+        for num in frequencyMap:
+                if num+1 in frequencyMap:
+                        current_length = frequencyMap[num] + frequencyMap[num+1]
+                        max_length = max(max_length, current_length)
+        return max_length
+
+
+print(solutionUsingHashMaps([1,3,2,2,5,2,3,7]))
+        
